52f43be568fff863eed638c60609daaee102d125
Meira Pentermann <meira@meirapentermann.com>
2017-07-09 14:12:08 -0600

    design main page, navbar, links, colors, no real content

---
+++ js/blog.js
@@ -0,0 +1,6 @@
+/* Javascript for Meira's Blog */	
+	
+document.getElementById("openWindow").onclick = openWindow;
+function openWindow(){
+var w = window.open("http://skilldistillery.com/", "", "width=800, height=500, left=300, top=200");
+}
